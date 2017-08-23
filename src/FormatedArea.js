7baa1e7a5d6621d3c02b284d0bcfcdf07085f393
Joey <elagunaj@gmail.com>
2017-08-23 14:28:18 -0700

    Create basic component structure

---
+++ src/FormatedArea.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FormatedArea  extends Component {
+  render() {
+    return (
+      <div>
+        inside FormattedArea
+      </div>
+    )
+  }
+}
+
+export default FormatedArea;
---
+++ src/InputArea.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InputArea extends Component {
+  render() {
+    return (
+      <div>
+        inside input area
+      </div>
+    )
+  }
+}
+
+export default InputArea
