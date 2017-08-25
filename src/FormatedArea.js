117d53e980881447120714289ffc45cfbdf6d6ac
Joey <elagunaj@gmail.com>
2017-08-24 22:24:11 -0700

    Implement frontend pattern to render formatted document

--- src/FormatedArea.js
+++ src/FormatedArea.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import './Format.css'
 
-class FormatedArea  extends Component {
+class FormatedArea extends Component {
+  constructor() {
+    super();
+    this.handleHover = this.handleHover.bind(this);
+    this.handleHoverLeave = this.handleHoverLeave.bind(this);
+  }
+
+  handleHover(data, key) {
+    this.props.hoverText(data, key)
+  }
+
+  handleHoverLeave(data, key) {
+    this.props.inavtiveHoverText(data, key)
+  }
+
   render() {
+    let words = this.props.text.map((word, key) => {
+      return (
+        <span className={word.classes.classes.join(' ')} key={key} data={word.classes.trailingWords.join(' ')} onMouseEnter={()=>{this.handleHover(word.classes.trailingWords, key)}} onMouseLeave={()=>{this.handleHoverLeave(word.classes.trailingWords, key)}}>
+          {word.phrase + ' '} 
+        </span>
+      )
+    });
     return (
       <div>
-        inside FormattedArea
+          {words}
       </div>
     )
   }
--- src/InputArea.js
+++ src/InputArea.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import * as utilites from './utilites.js';
 
 class InputArea extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
+    });
+    let updatedText = utilites.highlightText(e.target.value);
+    this.props.updateText(updatedText)
+  }
+
   render() {
     return (
       <div>
-        inside input area
+        <form>
+          <textarea value={this.state.text} onChange={this.handleChange}/>
+        </form>
       </div>
     )
   }
